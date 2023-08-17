ea6b048e24147b61ab0d31e046a91324a5045671
krsnamara <martinj.fitzpatrick@gmail.com>
2023-08-17 14:54:35 -0400

    building out info controller to use firebase uid and auth

--- controllers/info.js
+++ controllers/info.js
@@ -1,60 +1,62 @@
-const express = require('express')
-const info = express.Router()
-const Info = require('../models/infoModel')
+const express = require('express');
+const info = express.Router();
+const Info = require('../models/infoModel');
+const isAuthenticated = require('../utils/isAuth');
 
-info.get('/', async (req, res) => {
+info.get('/', isAuthenticated, async (req, res) => {
   try {
-    console.log('hi')
+    // console.log('hi')
     if (req.user) {
-      res.json(await Info.find())
+      res.json(await Info.find({ uid: req.user.uid }));
     } else {
-      res.json(await Info.find({ uid: null }))
+      res.json(await Info.find({ uid: null }));
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Failed to get data' })
+    console.log(error);
+    res.status(500).json({ error: 'Failed to get data' });
   }
-})
+});
 
-info.post('/', async (req, res) => {
+info.post('/', isAuthenticated, async (req, res) => {
   try {
-    const info = await Info.create(req.body)
-    res.json(info)
+    req.body.uid = req.user.uid;
+    const newInfo = await Info.create(req.body);
+    res.json(newInfo);
   } catch (error) {
-    console.log(error)
-    res.status(400).json(error)
+    console.log(error);
+    res.status(400).json(error);
   }
-})
+});
 
-info.put('/:id', async (req, res) => {
+info.delete('/:id', isAuthenticated, async (req, res) => {
   try {
-    const updatedInfo = await Info.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
-    })
-    res.json(updatedInfo)
+    res.json(await Info.findByIdAndRemove(req.params.id));
   } catch (error) {
-    console.log(error)
-    res.status(400).json(error)
+    res.status(400).json(error);
   }
-})
-info.delete('/:id', async (req, res) => {
+});
+
+info.put('/:id', isAuthenticated, async (req, res) => {
   try {
-    const deletedInfo = await Info.findByIdAndRemove(req.params.id)
-    res.json(deletedInfo)
+    req.body.uid = req.user.uid;
+    res.json(
+      await Info.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      })
+    );
   } catch (error) {
-    console.log(error)
-    res.status(400).json(error)
+    res.status(400).json(error);
   }
-})
+});
 
 info.get('/:id', async (req, res) => {
   try {
-    const info = await Info.findById(req.params.id)
-    res.json(info)
+    const info = await Info.findById(req.params.id);
+    res.json(info);
   } catch (error) {
-    console.log(error)
-    res.status(400).json(error)
+    console.log(error);
+    res.status(400).json(error);
   }
-})
+});
 
-module.exports = info
+module.exports = info;
